e53752514493627da88f47cce70b32ffe1877e63
Juliana Rousseau <juliana.salafia1@gmail.com>
2026-01-13 16:51:29 -0300

    fix: update home widget to use Placeholder and update dependencies in pubspec.lock

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Container(),
+      home: Placeholder(),
     );
   }
 }
