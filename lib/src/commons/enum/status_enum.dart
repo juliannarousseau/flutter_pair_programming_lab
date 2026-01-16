01da89a34dc7b056dde4ad61096fa9562a5b6a89
Juliana Rousseau <juliana.salafia1@gmail.com>
2026-01-16 18:10:54 -0300

    feat: add StatusEnum and TypeEnum for managing status and type values

---
+++ lib/src/commons/enum/status_enum.dart
@@ -0,0 +1,7 @@
+enum StatusEnum {
+  pending('pending'),
+  done('done');
+
+  final String value;
+  const StatusEnum([this.value = '']);
+}
---
+++ lib/src/commons/enum/type_enum.dart
@@ -0,0 +1,9 @@
+enum TypeEnum {
+  shareLink('share_link'),
+  postStatus('post_status'),
+  postSocial('post_social');
+
+  final String value;
+
+  const TypeEnum([this.value = '']);
+}
